4af81371efcd36950fb0c26ddf09e362f42fc410
mzen <mzen1980@gmail.com>
2020-12-16 10:43:04 +0300

    cleaned from unused imports

--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -1,9 +1,6 @@
-from datacenter.models import Passcard
-from datacenter.models import Visit
 from django.shortcuts import render
 from django.utils.timezone import localtime
-from datacenter.models import Passcard, Visit
-import datetime
+from datacenter.models import Visit
 
 
 def storage_information_view(request):
@@ -13,15 +10,14 @@ def storage_information_view(request):
         duration_seconds = visit.get_duration()
         duration = Visit.get_hours_minutes(duration_seconds)
         non_closed_visits.append(
-                                {
-                                "who_entered": visit.passcard.owner_name,
-                                "entered_at": localtime(value=visit.entered_at),
-                                "duration": duration,
-                                }
-                                )
-
+            {
+                "who_entered": visit.passcard.owner_name,
+                "entered_at": localtime(value=visit.entered_at),
+                "duration": duration,
+            }
+        )
 
     context = {
-        "non_closed_visits": non_closed_visits,  
+        "non_closed_visits": non_closed_visits,
     }
     return render(request, 'storage_information.html', context)
